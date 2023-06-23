0c4cfbe6f1d549d450fe1905e35c6121fb9c5af1
evans-nyang <nyanggithub@gmail.com>
2023-06-23 13:16:03 +0300

    Update processing files

--- jumiascraper/processing/normalization.py
+++ jumiascraper/processing/normalization.py
@@ -51,23 +51,41 @@ def normalize_official_store(official_store):
     # Convert the value to True if 'Official Store', else False
     return official_store == 'Official Store'
 
-normalized_data = []
+def clean_dataset(dataset):
+    for item in dataset:
+        item['item_url'] = normalize_url(item['href'])
+        item['data_id'] = normalize_dataid(item['data-id'])
+        item['brand'] = item['brand']
+        item['specs'] = item['name2']
+        item['price'] = normalize_price(item['price'])
+        item['old_price'] = normalize_price(item['old_price'])
+        item['discount'] = normalize_discount(item['discount'])
+        item['votes'] = normalize_votes(item['votes'])
+        item['stars'] = normalize_stars(item['stars'])
+        item['image_url'] = normalize_image_url(item['image_url'])
+        item['official_store'] = normalize_official_store(item['official_store'])
+
+def remove_duplicates(dataset):
+    seen = set()
+    new_dataset = []
+    for item in dataset:
+        if item['data_id'] not in seen:
+            seen.add(item['data_id'])
+            new_dataset.append(item)
+        elif item['data_id'] in seen:
+            print(f"Duplicate item found: {item['data_id']}")
+            dataset.remove(item)
+    return new_dataset
+
+def delete_columns(data, columns):
+    for item in data:
+        for column in columns:
+            item.pop(column, None)
+    return data
 
 def normalize_dataset(dataset):
-    for key, value in dataset.items():
-        normalized_item = {
-            'crawled_at': value['crawled_at'],
-            'item_url': value['href'],
-            'data_id': normalize_dataid(value['data-id']),
-            'brand': value['brand'],
-            'specs': value['name2'],
-            'price': normalize_price(value['price']),
-            'old_price': normalize_price(value['old_price']),
-            'discount': normalize_discount(value['discount']),
-            'votes': normalize_votes(value['votes']),
-            'stars': normalize_stars(value['stars']),
-            'image_url': value['image_url'],
-            'official_store': normalize_official_store(value['official_store'])
-        }
-        normalized_data.append(normalized_item)
+    normalized_data = [value for data in dataset for value in data.get('inform', {}).values() if isinstance(value, dict)]
+    clean_dataset(normalized_data)
+    delete_columns(normalized_data, ['name2', 'href', 'data-id'])
+    normalized_data = remove_duplicates(normalized_data)
     return normalized_data
