97452fe68985b799da1ee6142da4ce0aeb3af948
evans-nyang <nyanggithub@gmail.com>
2023-06-23 13:29:50 +0300

    Remove unused dirs

--- airflow/scripts/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-airflow db upgrade
-
-airflow users create -r Admin -u admin -p admin -e admin@example.com -f admin -l airflow
-# "$_AIRFLOW_WWW_USER_USERNAME" -p "$_AIRFLOW_WWW_USER_PASSWORD"
-
-airflow webserver
